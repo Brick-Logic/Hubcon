f39aae384676f134da2a94dbe1c529299330738a
Brick-Logic <68768025+Brick-Logic@users.noreply.github.com>
2026-04-18 04:56:59 -0300

    Added missing ByteMessage class

---
+++ Hubcon.Server.Abstractions/Models/ByteMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hubcon.Server.Abstractions.Models
+{
+    /// <summary>
+    /// Model for Hubcon WebSocket messages.
+    /// </summary>
+    /// <param name="Bytes">Compiled bytes to send through the WebSocket channel.</param>
+    /// <param name="CancellationToken">The origin operation's cancellation token.</param>
+    public record ByteMessage(byte[] Bytes, CancellationToken CancellationToken);
+}
