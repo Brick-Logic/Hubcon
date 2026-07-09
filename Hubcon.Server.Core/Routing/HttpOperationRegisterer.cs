d6c15b188364711ece18a641854627e98fb20037
Brick-Logic <68768025+Brick-Logic@users.noreply.github.com>
2026-07-09 20:02:56 -0300

    Fixed endpoint grouping in OpenAPI and removed old code

--- Hubcon.Server.Core/Routing/HttpOperationRegisterer.cs
+++ Hubcon.Server.Core/Routing/HttpOperationRegisterer.cs
@@ -79,7 +79,9 @@ namespace Hubcon.Server.Core.Routing
 
             var endpointGroup = EndpointGroups.GetOrAdd(blueprint.HttpEndpointGroupName!, x =>
             {
-                var group = app.MapGroup(x);
+                var group = app
+                    .MapGroup(x)
+                    .WithTags(x);
                 return group;
             });
 
@@ -404,111 +406,6 @@ namespace Hubcon.Server.Core.Routing
             context.Response.StatusCode = hubconResponse.StatusCode;
             context.Response.ContentType = "application/json";
             return hubconResponse;
-        }
-
-        private static Delegate CreateDelegate(MethodInfo methodInfo, Type wrapperType, bool isGet = false)
-        {
-            if (methodInfo == null)
-                throw new ArgumentNullException(nameof(methodInfo));
-
-            Type[] paramTypes;
-            var parameters = methodInfo.GetParameters();
-            if (parameters.Length > 0)
-            {
-                paramTypes = [wrapperType];
-            }
-            else
-            {
-                paramTypes = [.. parameters.Select(x => x.ParameterType)];
-            }
-
-            var (Instance, Method) = ProxyFactory.CreateProxyInstance(methodInfo, wrapperType, isGet);
-            var returnType = methodInfo.ReturnType;
-
-            if (paramTypes.Length > 16)
-                throw new NotSupportedException("Métodos con más de 16 parámetros no soportados.");
-
-            Type delegateType;
-
-            if (returnType == typeof(void))
-            {
-                // Action<T1,...,Tn>
-                delegateType = paramTypes.Length switch
-                {
-                    0 => typeof(Action),
-                    1 => typeof(Action<>).MakeGenericType(paramTypes),
-                    2 => typeof(Action<,>).MakeGenericType(paramTypes),
-                    3 => typeof(Action<,,>).MakeGenericType(paramTypes),
-                    4 => typeof(Action<,,,>).MakeGenericType(paramTypes),
-                    5 => typeof(Action<,,,,>).MakeGenericType(paramTypes),
-                    6 => typeof(Action<,,,,,>).MakeGenericType(paramTypes),
-                    7 => typeof(Action<,,,,,,>).MakeGenericType(paramTypes),
-                    8 => typeof(Action<,,,,,,,>).MakeGenericType(paramTypes),
-                    9 => typeof(Action<,,,,,,,,>).MakeGenericType(paramTypes),
-                    10 => typeof(Action<,,,,,,,,,>).MakeGenericType(paramTypes),
-                    11 => typeof(Action<,,,,,,,,,,>).MakeGenericType(paramTypes),
-                    12 => typeof(Action<,,,,,,,,,,,>).MakeGenericType(paramTypes),
-                    13 => typeof(Action<,,,,,,,,,,,,>).MakeGenericType(paramTypes),
-                    14 => typeof(Action<,,,,,,,,,,,,,>).MakeGenericType(paramTypes),
-                    15 => typeof(Action<,,,,,,,,,,,,,,>).MakeGenericType(paramTypes),
-                    16 => typeof(Action<,,,,,,,,,,,,,,,>).MakeGenericType(paramTypes),
-                    _ => throw new NotSupportedException()
-                };
-            }
-            else
-            {
-                // Func<T1,...,Tn,TResult>
-                Type[] typeArgs = paramTypes.Concat(new[] { returnType }).ToArray();
-                delegateType = paramTypes.Length switch
-                {
-                    0 => typeof(Func<>).MakeGenericType(typeArgs),
-                    1 => typeof(Func<,>).MakeGenericType(typeArgs),
-                    2 => typeof(Func<,,>).MakeGenericType(typeArgs),
-                    3 => typeof(Func<,,,>).MakeGenericType(typeArgs),
-                    4 => typeof(Func<,,,,>).MakeGenericType(typeArgs),
-                    5 => typeof(Func<,,,,,>).MakeGenericType(typeArgs),
-                    6 => typeof(Func<,,,,,,>).MakeGenericType(typeArgs),
-                    7 => typeof(Func<,,,,,,,>).MakeGenericType(typeArgs),
-                    8 => typeof(Func<,,,,,,,,>).MakeGenericType(typeArgs),
-                    9 => typeof(Func<,,,,,,,,,>).MakeGenericType(typeArgs),
-                    10 => typeof(Func<,,,,,,,,,,>).MakeGenericType(typeArgs),
-                    11 => typeof(Func<,,,,,,,,,,,>).MakeGenericType(typeArgs),
-                    12 => typeof(Func<,,,,,,,,,,,,>).MakeGenericType(typeArgs),
-                    13 => typeof(Func<,,,,,,,,,,,,,>).MakeGenericType(typeArgs),
-                    14 => typeof(Func<,,,,,,,,,,,,,,>).MakeGenericType(typeArgs),
-                    15 => typeof(Func<,,,,,,,,,,,,,,,>).MakeGenericType(typeArgs),
-                    16 => typeof(Func<,,,,,,,,,,,,,,,,>).MakeGenericType(typeArgs),
-                    _ => throw new NotSupportedException()
-                };
-            }
-
-            return Delegate.CreateDelegate(delegateType, Instance, Method);
-        }
-
-        public static Delegate CreateHandler(MethodInfo methodInfo, Type wrapperType)
-        {
-            // Definimos el parámetro de entrada: (WrapperType wrapper)
-            var wrapperParam = Expression.Parameter(wrapperType, "wrapper");
-
-            // Obtenemos la instancia del servicio (se asume que se pasa o se obtiene de DI)
-            // Para Minimal APIs, el primer parámetro suele ser el servicio
-            var serviceParam = Expression.Parameter(methodInfo.DeclaringType!, "service");
-
-            // Construimos los argumentos para llamar al método real: wrapper.Prop1, wrapper.Prop2...
-            var args = methodInfo.GetParameters().Select(p =>
-            {
-                var propInfo = wrapperType.GetProperty(p.Name!);
-                return Expression.Property(wrapperParam, propInfo!);
-            });
-
-            // Llamada al método: service.Method(wrapper.Prop1, ...)
-            var methodCall = Expression.Call(serviceParam, methodInfo, args);
-
-            // Convertimos el resultado a object (para manejar Task o void)
-            var conversion = Expression.Convert(methodCall, typeof(object));
-
-            // Creamos el delegado: (service, wrapper) => (object)service.Method(wrapper.Prop1, ...)
-            return Expression.Lambda(conversion, serviceParam, wrapperParam).Compile();
         }
     }
 }
